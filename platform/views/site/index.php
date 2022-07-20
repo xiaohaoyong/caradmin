418e91497d0ad77b4dcbb5bc909d55620748bce0
xiaohaoyong@126.com <xiaohaoyong@126.com>
2022-07-20 23:44:55 +0800

    123

--- platform/views/site/index.php
+++ platform/views/site/index.php
@@ -3,9 +3,7 @@
 /* @var $this yii\web\View */
 
 $this->title = '首页';
-platform\assets\IndexAsset::register($this);
-?>
-<?php
+
 $Business=\common\models\Business::findOne(['id'=>Yii::$app->user->identity->bus_id]);
 ?>
 <div class="site-index">
@@ -14,18 +12,34 @@ $Business=\common\models\Business::findOne(['id'=>Yii::$app->user->identity->bus
         <img src="http://web.wzgeek.com/<?=$Business->img?>" width="100%">
     </section>
     <script>
+
     </script>
 </div>
 <?php
-\common\models\Model::begin([
+\yii\bootstrap\Modal::begin([
     'id' => 'modal',
-    'header' => '<h4 class="modal-title">创建</h4>',
-    'footer' => '<a href="#" class="btn btn-primary" data-dismiss="modal">Close</a>',
+    'options'=>[
+        'data-backdrop'=>'static',//点击空白处不关闭弹窗
+        'data-keyboard'=>false,
+        'data-dismiss'=>false,
+    ],
 ]);
-echo 121212;
-\common\models\Model::end();
+?>
+<div class="modal-body">
+    <div style="text-align: center;line-height: 30px">开通时间：<?=$Business->starttime?></div>
+    <div style="text-align: center;line-height: 30px">结束时间：<?=$Business->endtime?></div>
+    <div style="text-align: center;line-height: 26px;font-size: 16px;margin-top: 50px">系统即将到期，为了不影响您的使用，<br>请联系管理员（13239283188）修改到期时间</div>
+</div>
+<?php
+$day=ceil((strtotime($Business->endtime)-time())/86400);
+\yii\bootstrap\Modal::end();
 $updateJs = <<<JS
-    jQuery("#modal").modal('show');
+    var day = {$day};
+    if(day < 30){
+        $("#modal").modal('show');
+    }
 JS;
 $this->registerJs($updateJs);
 ?>
+
+
